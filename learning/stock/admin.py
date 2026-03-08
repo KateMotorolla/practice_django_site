0b96e2fd44a5e6af893e8ba742f51d600a7d9dd5
Ekaterina Motora <keys-keys-03@mail.ru>
2026-03-08 18:55:19 +0300

    Создала сайт на Django

---
+++ learning/stock/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from stock.models import Stock
+from stock.models import Currency
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("ticker", "name", "description")
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    pass
